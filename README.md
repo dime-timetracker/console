f9ac5f5686701ad2715366c87941eda108e8fec4
Thomas Jez <thomas.jez@gmx.de>
2015-05-30 13:10:54 +0200

    switched from naked curl to guzzle

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Dime Console
 a command line frontend for the Dime timetracker 
 
-Its based on Symfony console and Guzzle (not yet)
+Its based on Symfony console (not yet) and Guzzle
--- src/DimeConsole.php
+++ src/DimeConsole.php
@@ -1,5 +1,6 @@
 <?php
 namespace DimeConsole;
+use GuzzleHttp\Client;
 
 class DimeConsole
 {
@@ -10,6 +11,12 @@ class DimeConsole
 
     protected $token;
 
+    protected $client;
+
+    public function __construct() {
+        $this->client = new Client();
+    }
+
     public function readConfig() {
         $configXml = simplexml_load_file('config.xml');
         $this->username = (string)$configXml->username;
@@ -19,48 +26,40 @@ class DimeConsole
 
     public function login() {
         $route = '/public/login';
-        $postData = array(
+        $postData = [
             'client' => $this->clientId,
             'password' => $this->password,
             'username' => $this->username,
-        );
+        ];
         $postString = json_encode($postData);
-        $s = curl_init();
-        $headers = array (
-            'Accept: application/json, text/*',
-            'Content-Type: application/json; charset=utf-8',
-            'Content-Length: ' . strlen($postString),
-        );
-        curl_setopt($s, CURLOPT_URL, $this->baseUrl . $route);
-        curl_setopt($s, CURLOPT_POST, 1);
-        curl_setopt($s, CURLOPT_POSTFIELDS, $postString);
-        curl_setopt($s, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($s, CURLOPT_HTTPHEADER, $headers);
-        $data = curl_exec($s);
-        $dataObject = json_decode($data);
+        $headers = [
+            'Accept' => 'application/json, text/*',
+            'Content-Type' => 'application/json; charset=utf-8',
+            'Content-Length' => strlen($postString),
+        ];
+
+        $response = $this->client->post($this->baseUrl . $route, ['headers' => $headers, 'body' => $postString]);
+        $dataObject = json_decode($response->getBody());
+
         $this->token = $dataObject->token;
-        curl_close($s);
     }
 
     public function showActivities() {
         $route = '/public/api/activity';
-        $credentials = array (
+        $credentials = [
             'user' => $this->username,
             'clientId' => $this->clientId,
             'token' => $this->token,
-        );
+        ];
         $authName = 'DimeTimetracker';
-        $headers = array (
-            'Authorization: ' .  $authName . ' ' . implode(',', $credentials),
-            'Accept: application/json, text/*',
-        );
-        $s = curl_init();
-        curl_setopt($s, CURLOPT_URL,$this->baseUrl . $route);
-        curl_setopt($s, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($s, CURLOPT_RETURNTRANSFER, true);
-        $data = curl_exec($s);
-        curl_close($s);
-        $activities = json_decode($data);
+        $headers = [
+            'Authorization' =>  $authName . ' ' . implode(',', $credentials),
+            'Accept' => 'application/json, text/*',
+        ];
+
+        $response = $this->client->get($this->baseUrl . $route, ['headers' => $headers]);
+        $activities = json_decode($response->getBody()->getContents());
+
         foreach ($activities as $activity) {
             echo $activity->description . PHP_EOL;
         }
